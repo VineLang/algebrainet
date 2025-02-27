abfecc8378f32047d45a1f13be2dd52751f9e46f
tjjfvi <t6@t6.fyi>
2025-02-27 15:44:49 -0500

    refactor Twist

--- Algebrainet/EqN/Twist.lean
+++ Algebrainet/EqN/Twist.lean
@@ -26,371 +26,211 @@ def twist_1_n : EqN S # (twist 1 n) (twist₁ n) := by
 def twist_1_1 : EqN S # (twist 1 1) swap := trans twist_1_n twist₁_1
 
 def twist₁_succ : EqN S # (twist₁ (n + 1)) (cat # (mix swap (wires n)) (mix wire (twist₁ n))) := by
-  induction n
-  apply trans twist₁_1
-  apply symm
-  apply trans
-  apply cat_
-  apply mix_nil
-  apply symm wires₂
-  simp
-  apply cat_wires
+  induction n with
+  | zero =>
+    apply trans twist₁_1
+    apply symm
+    apply trans (cat_ mix_nil wires₂.symm)
+    apply cat_wires
+    simp
+  | succ n ih =>
+    apply trans cast_
+    apply trans; apply cat₀; .
+      apply trans (mix_ ih wire_cat.symm)
+      apply exch.symm
+      repeat simp
+    simp
+    apply trans cat_assoc
+    apply cat_
+    . apply mix_assoc
+    . apply trans; apply cat_
+      . apply mix_assoc
+      . apply trans; apply mix₀; .
+          apply trans (wires_mix_wires_ (a := 1) (b := n)).symm
+          apply mix₀ wires₁
+          simp
+        apply mix_assoc
+      simp
+      apply trans exch
+      apply mix_ wire_cat cast_.symm
+      repeat simp
+    repeat simp
 
-  rename_i n ih
-  apply trans
-  apply trans cast_
-  apply cat₀
-  apply trans
-  apply mix₀
-  assumption
-  apply trans
-  apply mix₁
-  apply symm wire_cat
-  simp
-  apply symm exch
-  repeat simp
-  apply trans cat_assoc
-  apply cat_
-  apply mix_assoc
-  apply trans
-  apply cat_
-  apply mix_assoc
-  apply trans
-  apply mix₀
-  apply trans (symm (wires_mix_wires_ (a := 1) (b := n)))
-  apply mix₀
-  apply wires₁
-  simp
-  apply mix_assoc
-  simp
-  apply trans exch
-  apply mix_
-  apply wire_cat
-  apply symm
-  apply cast_
-  repeat simp
-
-def untwist₁ (n : Nat) : Net S (n + 1) (1 + n) := twist n 1
+abbrev untwist₁ (n : Nat) : Net S (n + 1) (1 + n) := twist n 1
 
 def untwist₁_succ : EqN S # (untwist₁ (n + 1)) (cat # (mix (wires n) swap) (mix (untwist₁ n) wire)) := by
-  induction n
-  simp only [twist₁, untwist₁, twist, wires]
-  apply trans cast_
-  apply trans
-  apply cat_
-  apply trans
-  apply mix₁
-  apply cast_
-  apply trans
-  apply mix₀
-  apply symm wires₁
-  apply wires_mix_wires
-  simp
-  apply trans mix_nil
-  apply twist₁_1
-  simp
-  apply trans wires_cat
-  apply symm
-  apply trans
-  apply cat_
-  apply nil_mix
-  apply mix₀
-  apply cast_
-  simp
-  apply cat_wires
-
-  rename_i n ih
-  apply trans cast_
-  apply trans
-  apply cat_
-  apply trans
-  apply mix_
-  apply symm cat_wire
-  simp
-  assumption
-  apply trans
-  apply symm exch
-  simp
-  apply cat₀
-  apply trans
-  apply symm mix_assoc
-  apply mix₀
-  apply refl
-  repeat simp
-  apply refl
-  simp
-  apply trans
-  apply cat_assoc
-  repeat simp
-  apply cat_
-  apply mix₀
-  apply trans
-  apply mix₀
-  apply symm wires₁
-  apply wires_mix_wires_
-  simp
-  apply trans
-  apply cat_
-  apply symm mix_assoc
-  apply symm mix_assoc
-  simp
-  apply trans exch
-  apply mix_
-  apply symm
-  apply cast_
-  simp
-  apply wire_cat
-  repeat simp
-
+  induction n with
+  | zero =>
+    apply trans cast_
+    apply trans; apply cat_
+    . apply trans (mix₁ cast_)
+      apply wire_mix_wires
+    . apply trans mix_nil twist₁_1
+    simp
+    apply trans wires_cat
+    apply symm
+    apply trans (cat_ nil_mix (mix₀ cast_))
+    apply cat_wires
+    simp
+  | succ n ih =>
+    apply trans cast_
+    apply trans; apply cat₀; .
+      apply trans (mix_ cat_wire.symm ih)
+      apply trans exch.symm
+      apply cat₀ mix_assoc.symm
+      repeat simp
+    simp
+    apply trans cat_assoc
+    apply cat_
+    . apply mix₀ wire_mix_wires
+    . apply trans (cat_ mix_assoc.symm mix_assoc.symm)
+      apply trans exch
+      apply mix_ cast_.symm wire_cat
+      repeat simp
+    simp
 
 def twist₁_untwist₁ : EqN S # (cat # (twist₁ n) (untwist₁ n)) (wires (n + 1)) := by
-  induction n
-  simp only [twist₁, untwist₁, twist, wires]
-  apply trans wire_cat
-  apply cast_
-
-  rename_i n ih
-  apply trans
-  simp only [twist₁]
-  apply cat_
-  apply cast_
-  apply untwist₁_succ
-  simp
-  apply trans cat_assoc
-  apply trans
-  apply cat₁
-  apply trans (symm cat_assoc)
-  apply trans
-  apply cat₀
-  apply trans exch
-  apply trans
-  apply mix_
-  apply wires_cat
-  apply swap_swap
-  apply trans
-  apply mix₁
-  apply symm wires₂
-  apply wires_mix_wires
-  repeat simp
-  apply wires_cat
-  repeat simp
-  apply trans exch
-  apply mix_
-  assumption
-  apply wire_cat
-  repeat simp
+  induction n with
+  | zero => apply trans wire_cat cast_
+  | succ n ih =>
+    apply trans (cat_ cast_ untwist₁_succ)
+    apply trans cat_assoc
+    apply trans; apply cat₁; .
+      apply trans cat_assoc.symm
+      apply trans; apply cat₀; .
+        apply trans exch
+        apply trans (mix_ wires_cat swap_swap)
+        apply trans (mix₁ wires₂.symm)
+        apply wires_mix_wires
+        repeat simp
+      simp
+      apply wires_cat
+      repeat simp
+    simp
+    apply trans exch
+    apply mix_ ih wire_cat
+    repeat simp
 
 def twist_n_0 : EqN S # (twist n 0) (wires n) := by
-  induction n
-  exact cast_
-  rename_i n ih
-  simp only [twist]
-  apply trans cast_
-  apply trans
-  apply cat_
-  apply trans
-  apply mix_
-  exact symm wires₁
-  assumption
-  apply wires_mix_wires
-  simp
-  apply trans
-  apply mix₀
-  exact symm wires₁
-  apply wires_mix_wires_ (n := n + 1)
-  repeat simp
-  apply wires_cat
+  induction n with
+  | zero => apply cast_
+  | succ n ih =>
+    apply trans cast_
+    apply trans; apply cat_
+    . apply trans (mix₁ ih)
+      apply wire_mix_wires
+    . apply wire_mix_wires
+    simp
+    apply wires_cat
 
 def twist_s_n : EqN S # (twist (a + 1) b) (cat # (mix (wires a) (twist₁ b)) (mix (twist a b) wire)) := by
-  induction a
-  simp only [twist, twist₁, wires]
-  apply trans cast_
-  apply trans
-  apply cat_
-  apply trans (mix_ (symm wires₁) cast_)
-  apply wires_mix_wires
-  simp
-  apply mix_nil
-  simp
-  apply trans wires_cat
-  apply symm
-  apply trans
-  apply cat_
-  apply nil_mix
-  apply mix₀ cast_
-  simp
-  apply cat_wires
-
-  rename_i a ih
-  apply trans cast_
-  apply trans
-  apply cat₀
-  apply trans
-  apply mix_
-  apply symm wire_cat
-  simp
-  assumption
-  apply symm exch
-  repeat simp
-  apply trans cat_assoc
-  apply cat_
-  apply trans (symm mix_assoc)
-  apply mix₀
-  apply trans (mix₀ (symm wires₁))
-  apply wires_mix_wires_
-  simp
-  apply trans
-  apply cat_
-  apply symm mix_assoc
-  apply symm mix_assoc
-  simp
-  apply trans exch
-  apply mix_
-  apply symm
-  apply cast_
-  apply wire_cat
-  repeat simp
+  induction a with
+  | zero =>
+    apply trans cast_
+    apply trans; apply cat_
+    . apply trans (mix₁ cast_)
+      apply wire_mix_wires
+    . apply mix_nil
+    simp
+    apply trans wires_cat
+    apply symm
+    apply trans (cat_ nil_mix (mix₀ cast_))
+    apply cat_wires
+    simp
+  | succ a ih =>
+    apply trans cast_
+    apply trans; apply cat₀
+    . apply trans (mix_ wire_cat.symm ih)
+      apply exch.symm
+      repeat simp
+    simp
+    apply trans cat_assoc
+    apply cat_
+    . apply trans mix_assoc.symm
+      apply mix₀ wire_mix_wires
+    . apply trans (cat_ mix_assoc.symm mix_assoc.symm)
+      apply trans exch
+      apply mix_ cast_.symm wire_cat
+      repeat simp
+    simp
 
 def twist_n_s : EqN S # (twist a (b + 1)) (cat # (mix (twist a b) wire) (mix (wires b) (untwist₁ a))) := by
-  revert b
-  induction a
-  intro b
-  simp only [twist, wires, untwist₁]
-  apply trans cast_
-  apply symm
-  apply trans
-  apply cat_
-  apply mix₀ (cast_)
-  apply trans (mix₁ cast_)
-  apply wires_mix_wires
-  repeat simp
-  apply wires_cat
-
-  rename_i a ih
-  intro b
-  apply trans
-  apply twist_s_n
-  apply trans
-  apply cat₁
-  apply mix₀
-  apply ih
-  simp
-  apply trans
-  apply cat₁
-  apply trans
-  apply mix₁
-  apply symm wire_cat
-  simp
-  apply symm exch
-  repeat simp
-  apply trans (symm cat_assoc)
-  apply trans
-  apply cat₀
-  apply trans
-  apply cat₀
-  apply trans
-  apply mix_
-  apply symm (wires_cat (n := a))
-  simp
-  apply cast_
-  apply trans (symm exch)
-  apply cat₁
-  apply trans (symm mix_assoc)
-  apply mix₀ wires_mix_wires
-  repeat simp
-  apply trans
-  apply cat₁
-  apply mix_assoc
-  simp
-  apply trans cat_assoc
-  apply trans
-  apply cat₁
-  apply trans exch
-  apply trans
-  apply mix_
-  apply trans wires_cat
-  apply symm (cat_wires (n := a + b))
-  repeat simp
-  apply trans (cat₁ (symm wires₂))
-  apply trans cat_wires
-  apply symm (wires_cat (n := 2))
-  repeat simp
-  apply symm exch
-  repeat simp
-  apply trans (symm (cat_assoc))
-  apply cat₀
-  apply trans
-  apply cat_
-  apply symm mix_assoc
-  apply symm (mix_assoc (z := wire))
-  repeat simp
-  apply trans exch
-  apply mix_
-  apply trans
-  apply cat₁
-  apply mix₁
-  apply wires₁
-  simp
-  apply symm
-  apply twist_s_n
-  apply wire_cat
-  repeat simp
-  apply trans cat_assoc
-  apply cat₁
-  apply trans
-  apply cat₀
-  apply mix₀
-  apply symm (wires_mix_wires_ (a := b) (b := a))
-  repeat simp
-  apply trans
-  apply cat_
-  apply mix_assoc
-  apply mix_assoc
-  repeat simp
-  apply trans exch
-  apply mix_
-  apply cat_wires
-  apply symm
-  apply untwist₁_succ
-  repeat simp
+  induction a with
+  | zero =>
+    apply trans cast_
+    apply symm
+    apply trans; apply cat_
+    . apply mix₀ cast_
+    . apply trans (mix₁ cast_)
+      apply wires_mix_wires
+    simp
+    apply wires_cat
+  | succ a ih =>
+    apply trans twist_s_n
+    apply trans; apply cat₁; .
+      apply trans (mix_ ih wire_cat.symm)
+      apply exch.symm
+      repeat simp
+    simp
+    apply trans cat_assoc.symm
+    apply trans; apply cat₀; .
+      apply trans; apply cat_
+      . apply trans (mix_ (wires_cat (n := a)).symm cast_)
+        apply trans exch.symm
+        apply cat₁
+        apply trans mix_assoc.symm
+        apply mix₀ wires_mix_wires
+        repeat simp
+      . apply mix_assoc
+      simp
+      apply trans cat_assoc
+      apply trans; apply cat₁; .
+        apply trans (cat₁ (mix₁ wires₂.symm))
+        apply trans up_down.symm down_up
+        simp
+      simp
+      apply trans cat_assoc.symm
+      apply cat₀
+      apply trans (cat_ mix_assoc.symm (mix_assoc (z := wire)).symm)
+      apply trans exch
+      apply mix_
+      . apply trans (cat₁ (mix₁ wires₁))
+        apply twist_s_n.symm
+        simp
+      . apply wire_cat
+      repeat simp
+    simp
+    apply trans cat_assoc
+    apply cat₁
+    apply trans (cat₀ (mix₀ (wires_mix_wires_ (a := b) (b := a)).symm))
+    apply trans (cat_ mix_assoc mix_assoc)
+    apply trans exch
+    apply mix_ cat_wires untwist₁_succ.symm
+    repeat simp
 
 def twist_untwist : EqN S # (cat # (twist a b) (twist b a)) (wires (a + b)) := by
-  induction a
-  apply trans
-  apply cat_
-  apply cast_
-  apply twist_n_0
-  simp
-  apply trans cat_wires
-  apply wires_
-  simp
-
-  rename_i a ih
-  apply trans
-  apply cat_
-  apply twist_s_n
-  apply twist_n_s
-  simp
-  apply trans cat_assoc
-  apply trans
-  apply cat₁
-  apply trans (symm cat_assoc)
-  apply trans
-  apply cat₀
-  apply trans exch
-  apply mix_
-  assumption
-  apply wire_cat
-  repeat simp
-  apply wires_cat
-  repeat simp
-  apply trans exch
-  apply trans
-  apply mix_
-  apply wires_cat
-  apply twist₁_untwist₁
-  apply wires_mix_wires_
-  repeat simp
+  induction a with
+  | zero =>
+    apply trans (cat_ cast_ twist_n_0)
+    apply trans cat_wires wires_
+    repeat simp
+  | succ a ih =>
+    apply trans (cat_ twist_s_n twist_n_s)
+    apply trans cat_assoc
+    apply trans; apply cat₁; .
+      apply trans cat_assoc.symm
+      apply trans; apply cat₀; .
+        apply trans exch
+        apply mix_ ih wire_cat
+        repeat simp
+      simp
+      apply wires_cat
+      repeat simp
+    simp
+    apply trans exch
+    apply trans (mix_ wires_cat twist₁_untwist₁)
+    apply wires_mix_wires_
+    repeat simp
 
 def twist_add_n : EqN S # (twist (a + b) n) (cat # (mix (wires a) (twist b n)) (mix (twist a n) (wires b))) := by
   induction b with
