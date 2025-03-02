08033baa8821cb4da3ff3096999ca4151cfcb899
tjjfvi <t6@t6.fyi>
2025-03-02 08:46:14 -0500

    improve EqN handling

--- Algebrainet/Net.lean
+++ Algebrainet/Net.lean
@@ -36,3 +36,62 @@ def twist₁ : (n : Nat) -> Net S (1 + n) (n + 1)
 def twist : (a b : Nat) -> Net S (a + b) (b + a)
   | 0, b => (castₙ # (wires b))
   | a + 1, b => (castₙ # (cat # (mix wire (twist a b)) (mix (twist₁ b) (wires a))))
+
+namespace Net
+
+def kind {i o : Nat} : Net S i o -> Nat
+| nil => 0
+| wire => 1
+| swap => 2
+| cup => 3
+| cap => 4
+| agent _ => 5
+| mix _ _ => 6
+| cat _ _ _ => 7
+
+end Net
+
+abbrev ANet (S : System) := Σ i o, Net S i o
+
+abbrev EqN {S : System} {aᵢ aₒ bᵢ bₒ : Nat} (a : Net S aᵢ aₒ) (b : Net S bᵢ bₒ) : Prop
+  := Eq (α := ANet S) ⟨aᵢ, aₒ, a⟩ ⟨bᵢ, bₒ, b⟩
+
+namespace EqN
+
+variable
+  {S : System}
+  {aᵢ aₒ : Nat} {a : Net S aᵢ aₒ}
+  {bᵢ bₒ : Nat} {b : Net S bᵢ bₒ}
+  {cᵢ cₒ : Nat} {c : Net S cᵢ cₒ}
+  {dᵢ dₒ : Nat} {d : Net S dᵢ dₒ}
+
+def congr
+  (h : EqN a b)
+  (α : Type)
+  (f : {i o : Nat} -> Net S i o -> α)
+  : f a = f b
+:= Eq.rec (motive := fun b _ => f a = f b.snd.snd) rfl h
+
+def i (e : EqN a b) : aᵢ = bᵢ := e.congr Nat (fun {i _} _ => i)
+def o (e : EqN a b) : aₒ = bₒ := e.congr Nat (fun {_ o} _ => o)
+
+abbrev unsome {α : Type} {a b : α} : some a = some b -> a = b := Option.some_inj.mp
+
+def cat₀ {h₀ h₁ : _} (e : EqN (cat h₀ a b) (cat h₁ c d)) : EqN a c :=
+  unsome (e.congr (Option (ANet S)) fun | cat _ n _ => some ⟨_, _, n⟩ | _ => none)
+
+def cat₁ {h₀ h₁ : _} (e : EqN (cat h₀ a b) (cat h₁ c d)) : EqN b d :=
+  unsome (e.congr (Option (ANet S)) fun | cat _ _ n => some ⟨_, _, n⟩ | _ => none)
+
+def mix₀ (e : EqN (mix a b) (mix c d)) : EqN a c :=
+  unsome (e.congr (Option (ANet S)) fun | mix n _ => some ⟨_, _, n⟩ | _ => none)
+
+def mix₁ (e : EqN (mix a b) (mix c d)) : EqN b d :=
+  unsome (e.congr (Option (ANet S)) fun | mix  _ n => some ⟨_, _, n⟩ | _ => none)
+
+def agent {a b : S.Agent} (e : EqN (agent a) (agent b)) : a = b:=
+  unsome (e.congr (Option S.Agent) fun | .agent a => some a | _ => none)
+
+def kind (e : EqN a b) : a.kind = b.kind := (e.congr Nat Net.kind)
+
+end EqN
--- Algebrainet/Rd.lean
+++ Algebrainet/Rd.lean
@@ -45,145 +45,68 @@ inductive Vee (R : Rules S) {i o : Nat} (b c : Net S i o) : Prop
   | eq : (b = c) -> Vee R b c
   | rd : {d : Net S i o} -> Rd S R b d -> Rd S R c d -> Vee R b c
 
-abbrev EqN {S : System} {aᵢ aₒ bᵢ bₒ : Nat} (a : Net S aᵢ aₒ) (b : Net S bᵢ bₒ) : Prop
-  := Eq (α := Σ i o, Net S i o) ⟨aᵢ, aₒ, a⟩ ⟨bᵢ, bₒ, b⟩
-
-def extract_agent {i o : Nat} : (n : Net S i o) -> Option S.Agent
-  | agent a => some a
-  | _ => none
-
-namespace EqN
-
-variable
-  {aᵢ aₒ bᵢ bₒ : Nat} {a : Net S aᵢ aₒ} {b : Net S bᵢ bₒ}
-
-def congr
-  {α : Type}
-  (h : EqN a b)
-  (f : {i o : Nat} -> Net S i o -> α)
-  : f a = f b
-:= Eq.rec (motive := fun b _ => f a = f b.snd.snd) rfl h
-
-@[simp] def eqᵢ (h : EqN a b) : aᵢ = bᵢ := Eq.rec (motive := fun b _ => aᵢ = b.fst) rfl h
-@[simp] def eqₒ (h : EqN a b) : aₒ = bₒ := Eq.rec (motive := fun b _ => aₒ = b.snd.fst) rfl h
-
-end EqN
-
-abbrev SNet (S : System) := Σ i o, Net S i o
-
-def unpair {i o : Nat} : (n : Net S i o) -> Option (S.Agent × S.Agent)
-  | cat _ (mix (agent a) (agent b)) cap => some (a, b)
-  | _ => none
-
-def uncat₀ {i o : Nat} : (n : Net S i o) -> Option (SNet S)
-  | cat _ a _ => some ⟨_, _, a⟩
-  | _ => none
-
-def uncat₁ {i o : Nat} : (n : Net S i o) -> Option (SNet S)
-  | cat _ _ b => some ⟨_, _, b⟩
-  | _ => none
-
-def unmix₀ {i o : Nat} : (n : Net S i o) -> Option (SNet S)
-  | mix a _ => some ⟨_, _, a⟩
-  | _ => none
-
-def unmix₁ {i o : Nat} : (n : Net S i o) -> Option (SNet S)
-  | mix _ b => some ⟨_, _, b⟩
-  | _ => none
-
-def kind {i o : Nat} : (n : Net S i o) -> Nat
-  | nil => 0
-  | wire => 1
-  | swap => 2
-  | cup => 3
-  | cap => 4
-  | agent _ => 5
-  | mix _ _ => 6
-  | cat _ _ _ => 7
-
-def cat₀_kind {i o : Nat} : (n : Net S i o) -> Option (Nat)
-  | cat _ a _ => some (kind a)
-  | _ => none
-
-def cat₁_kind {i o : Nat} : (n : Net S i o) -> Option (Nat)
-  | cat _ _ b => some (kind b)
-  | _ => none
-
-def cat₀_mix₀_kind {i o : Nat} : (n : Net S i o) -> Option (Nat)
-  | cat _ (mix a _) _ => some (kind a)
-  | _ => none
-
-def cat₀_mix₁_kind {i o : Nat} : (n : Net S i o) -> Option (Nat)
-  | cat _ (mix _ b) _ => some (kind b)
-  | _ => none
-
-abbrev unsome {α : Type} {a b : α} : some a = some b -> a = b := Option.some_inj.mp
-
 def rd_perf_conf_ {S : System} {R : Rules S}
   {i₀ o₀ i₁ o₁ : Nat}
   {a₀ b : Net S i₀ o₀} {a₁ c : Net S i₁ o₁}
   (r₀ : Rd S R a₀ b) (r₁ : Rd S R a₁ c)
   (hₐ : EqN a₀ a₁)
-  : Vee R (castₙ ⟨hₐ.eqᵢ, hₐ.eqₒ⟩ b) c
+  : Vee R (castₙ ⟨hₐ.i, hₐ.o⟩ b) c
 := by
-  induction r₀ generalizing i₁ o₁ a₁ c r₁ with
+  induction r₀ generalizing i₁ o₁ with
   | pair x₀ y₀ d₀ =>
     cases r₁ with
     | pair x₁ y₁ d₁ =>
-      cases hₐ.congr unpair
+      cases hₐ.cat₀.mix₀.agent
+      cases hₐ.cat₀.mix₁.agent
       apply Vee.eq
       simp
-    | mix₀ | mix₁ => repeat cases hₐ.congr kind
+    | mix₀ | mix₁ => repeat cases hₐ.kind
     | cat₀ r₁ =>
-      cases r₁
-      repeat . cases hₐ.congr cat₀_kind
-      repeat {
-        rename_i r₁ _; cases r₁
-        repeat . cases hₐ.congr cat₀_mix₀_kind
-        repeat . cases hₐ.congr cat₀_mix₁_kind
-      }
-    | cat₁ r₁ => cases r₁; repeat cases hₐ.congr cat₁_kind
+      cases r₁ <;> first
+      | . cases hₐ.cat₀.kind
+      | . rename_i r₁ _; cases r₁ <;> first
+          | . cases hₐ.cat₀.mix₀.kind
+          | . cases hₐ.cat₀.mix₁.kind
+    | cat₁ r₁ => cases r₁ <;> cases hₐ.cat₁.kind
   | cat₀ r₀ ih =>
     cases r₁ with
     | cat₀ r₁ =>
-      cases ih r₁ (hₐ := unsome (hₐ.congr uncat₀)) with
+      cases ih r₁ hₐ.cat₀ with
       | eq h => apply Vee.eq; cases h; cases hₐ; rfl
       | rd l r => cases hₐ; exact Vee.rd (.cat₀ l) (.cat₀ r)
     | cat₁ r₁ => cases hₐ; simp; exact Vee.rd (.cat₁ r₁) (.cat₀ r₀)
-    | mix₀ | mix₁ => repeat cases hₐ.congr kind
+    | mix₀ | mix₁ => cases hₐ.kind
     | pair =>
-      cases r₀
-      repeat . cases hₐ.congr cat₀_kind
-      repeat {
-        rename_i r₁ _; cases r₁
-        repeat . cases hₐ.congr cat₀_mix₀_kind
-        repeat . cases hₐ.congr cat₀_mix₁_kind
-      }
+      cases r₀ <;> first
+      | . cases hₐ.cat₀.kind
+      | . rename_i r₁ _; cases r₁ <;> first
+          | . cases hₐ.cat₀.mix₀.kind
+          | . cases hₐ.cat₀.mix₁.kind
   | cat₁ r₀ ih =>
     cases r₁ with
     | cat₁ r₁ =>
-      cases ih r₁ (hₐ := unsome (hₐ.congr uncat₁)) with
+      cases ih r₁ hₐ.cat₁ with
       | eq h => apply Vee.eq; cases h; cases hₐ; rfl
       | rd l r => cases hₐ; exact Vee.rd (.cat₁ l) (.cat₁ r)
     | cat₀ r₁ => cases hₐ; simp; exact Vee.rd (.cat₀ r₁) (.cat₁ r₀)
-    | mix₀ | mix₁ => repeat cases hₐ.congr kind
-    | pair => cases r₀; repeat . cases hₐ.congr cat₁_kind
+    | mix₀ | mix₁ => cases hₐ.kind
+    | pair => cases r₀ <;> cases hₐ.cat₁.kind
   | mix₀ r₀ ih =>
     cases r₁ with
     | mix₀ r₁ =>
-      cases ih r₁ (hₐ := unsome (hₐ.congr unmix₀)) with
-      | eq h => apply Vee.eq; cases h; cases hₐ.congr unmix₀; cases hₐ.congr unmix₁; cases hₐ; rfl
-      | rd l r => cases hₐ.congr unmix₀; cases hₐ.congr unmix₁; cases hₐ; exact Vee.rd (.mix₀ l) (.mix₀ r)
-    | mix₁ r₁ => cases hₐ.congr unmix₀; cases hₐ.congr unmix₁; cases hₐ; simp; exact Vee.rd (.mix₁ r₁) (.mix₀ r₀)
-    | cat₀ | cat₁ | pair => repeat cases hₐ.congr kind
+      cases ih r₁ hₐ.mix₀ with
+      | eq h => apply Vee.eq; cases h; cases hₐ.mix₀; cases hₐ.mix₁; cases hₐ; rfl
+      | rd l r => cases hₐ.mix₀; cases hₐ.mix₁; cases hₐ; exact Vee.rd (.mix₀ l) (.mix₀ r)
+    | mix₁ r₁ => cases hₐ.mix₀; cases hₐ.mix₁; cases hₐ; simp; exact Vee.rd (.mix₁ r₁) (.mix₀ r₀)
+    | cat₀ | cat₁ | pair => repeat cases hₐ.kind
   | mix₁ r₀ ih =>
     cases r₁ with
     | mix₁ r₁ =>
-      cases ih r₁ (hₐ := unsome (hₐ.congr unmix₁)) with
-      | eq h => apply Vee.eq; cases h; cases hₐ.congr unmix₀; cases hₐ.congr unmix₁; cases hₐ; rfl
-      | rd l r => cases hₐ.congr unmix₀; cases hₐ.congr unmix₁; cases hₐ; exact Vee.rd (.mix₁ l) (.mix₁ r)
-    | mix₀ r₁ => cases hₐ.congr unmix₀; cases hₐ.congr unmix₁; cases hₐ; simp; exact Vee.rd (.mix₀ r₁) (.mix₁ r₀)
-    | cat₀ | cat₁ | pair => repeat cases hₐ.congr kind
+      cases ih r₁ hₐ.mix₁ with
+      | eq h => apply Vee.eq; cases h; cases hₐ.mix₀; cases hₐ.mix₁; cases hₐ; rfl
+      | rd l r => cases hₐ.mix₀; cases hₐ.mix₁; cases hₐ; exact Vee.rd (.mix₁ l) (.mix₁ r)
+    | mix₀ r₁ => cases hₐ.mix₀; cases hₐ.mix₁; cases hₐ; simp; exact Vee.rd (.mix₀ r₁) (.mix₁ r₀)
+    | cat₀ | cat₁ | pair => repeat cases hₐ.kind
 
 def rd_perf_conf
   {i o : Nat}
